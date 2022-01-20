5710794390ce12b4d6c9e60cd53700982745320d
hekerbewuga <95267581+hekerbewuga@users.noreply.github.com>
2022-01-20 22:19:33 +0530

    Just added a new command
    there is still a bug pls tryna fix it

--- Discord.py
+++ Discord.py
@@ -1,3 +1,6 @@
+from email import message
+from importlib.resources import contents
+from socket import MSG_CMSG_CLOEXEC, MSG_NOTIFICATION
 import discord
 import random
 import time
@@ -21,15 +24,18 @@ async def on_message(message):
         return
 
     if message.channel.name == 'bot-test':
-        if user_message == 'jc!ping':
+        if user_message == 'b! ping':
             start = time.time()
             loading_msg = await message.channel.send(f'Calculating ping...')
             finish = time.time()
             ping_ms = int((finish - start) * 1000)
             await loading_msg.edit(content=f':ping_pong: Pong! in {ping_ms}ms')
-        elif user_message.lower() == 'bye':
+        elif user_message.lower() == 'b! bye':
             await message.channel.send(f'Ta-Ta See you later {username}!')
-        elif user_message == 'jc!random':
+        elif user_message == 'b! random':
                 await message.reply(content=f'This is your random number {random.randint(0, 10)}')
-    
+
+user_message = MSG_NOTIFICATION.content
+if user_message == 'b!beg':
+    MSG_CMSG_CLOEXEC.edit(content=f'No coins for you LOL') 
 client.run(TOKEN)   
